4143830c63fff4aebd0304c97772441d4937b243
Stepan Shtennikov <shtennikov.s@gmail.com>
2023-09-07 10:17:34 +0500

    Add solution to VueCalculator problem

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,33 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const calculatorOptions = {
+  data() {
+    return {
+      numberLeft: null,
+      numberRight: null,
+      pickedOperator: null,
+    };
+  },
+  computed: {
+    result() {
+      switch (this.pickedOperator) {
+        case 'sum':
+          return this.numberLeft + this.numberRight;
+        case 'subtract':
+          return this.numberLeft - this.numberRight;
+        case 'multiply':
+          return this.numberLeft * this.numberRight;
+        case 'divide':
+          return this.numberLeft / this.numberRight;
+        default:
+          return 0;
+      }
+    },
+  },
+  template: '#calculator',
+};
+
+const Calculator = defineComponent(calculatorOptions);
+const app = createApp(Calculator);
+
+app.mount('#app');
