7bdbba0dc054417b866ea81041057473337c3b04
Stepan Shtennikov <shtennikov.s@gmail.com>
2023-09-27 18:02:39 +0500

    Add solution to MeetupsCalendar problem

---
+++ 04-vue-cli/04-MeetupsCalendar/components/UiCalendarDays.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="calendar-view__grid">
+    <UiCalendarCell
+      v-for="(day, index) in daysList"
+      :key="index"
+      :day="day.day"
+      :is-active="day.isActive"
+      :events="day.events"
+    />
+  </div>
+</template>
+
+<script>
+import UiCalendarCell from './UiCalendarCell.vue';
+
+export default {
+  name: 'UiCalendarDays',
+
+  components: {
+    UiCalendarCell,
+  },
+
+  props: {
+    daysList: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.calendar-view__grid {
+  display: grid;
+  grid-template-columns: repeat(1, 1fr);
+}
+
+.calendar-view__grid {
+  border: 1px solid var(--grey);
+  border-bottom: none;
+}
+
+@media all and (min-width: 767px) {
+  .calendar-view__grid {
+    grid-template-columns: repeat(7, 1fr);
+  }
+}
+</style>
