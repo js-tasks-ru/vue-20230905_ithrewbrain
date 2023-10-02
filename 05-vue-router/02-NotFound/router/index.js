14872cf7bb992b251869c96bfb7d3a2e15e8b06f
Stepan Shtennikov <shtennikov.s@gmail.com>
2023-10-02 12:48:46 +0500

    Add solution to NotFound problem

--- 05-vue-router/02-NotFound/router/index.js
+++ 05-vue-router/02-NotFound/router/index.js
@@ -12,5 +12,9 @@ export const router = createRouter({
       path: '/page-b',
       component: () => import('../views/PageB'),
     },
+    {
+      path: '/:catchAll(.*)*',
+      component: () => import('../views/PageNotFound'),
+    },
   ],
 });
