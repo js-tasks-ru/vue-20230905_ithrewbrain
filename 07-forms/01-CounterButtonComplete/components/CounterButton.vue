a390e601b3e788db545474850df7488391587f4e
Stepan Shtennikov <shtennikov.s@gmail.com>
2023-10-11 13:56:40 +0500

    Add solution to CounterButtonComplete problem

--- 07-forms/01-CounterButtonComplete/components/CounterButton.vue
+++ 07-forms/01-CounterButtonComplete/components/CounterButton.vue
@@ -1,9 +1,35 @@
 <template>
-  <button></button>
+  <button @click="$emit('update:count', ++localCount)">
+    {{ localCount }}
+  </button>
 </template>
 
 <script>
 export default {
   name: 'CounterButton',
+
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+    },
+  },
+
+  emits: ['update:count'],
+
+  data() {
+    return {
+      localCount: null,
+    };
+  },
+
+  watch: {
+    count: {
+      immediate: true,
+      handler(newValue) {
+        this.localCount = newValue;
+      },
+    },
+  },
 };
 </script>
