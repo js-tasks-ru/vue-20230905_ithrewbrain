cf28f85b444ef19b6118ab85da7920d64210ce57
Stepan Shtennikov <shtennikov.s@gmail.com>
2023-10-28 17:06:39 +0500

    Add solution to UiPanes-JSX problem

--- 09-rendering/03-UiPanes-JSX/components/UiPanes.vue
+++ 09-rendering/03-UiPanes-JSX/components/UiPanes.vue
@@ -1,40 +1,3 @@
-<template>
-  <div class="panes">
-    <!--  Pane 0 -->
-    <div class="pane">
-      <div class="pane__content">
-        <!-- Определяем, какой именно слот рендерить по массиву порядка панелей -->
-        <slot :name="`pane-${panes[0]}`" />
-      </div>
-      <div class="pane__controls">
-        <!-- Скрываем кнопку классом pane__disabled-button -->
-        <UiButton class="pane__disabled-button" variant="secondary" block @click="up(0)"> Up </UiButton>
-        <UiButton variant="danger" block @click="down(0)"> Down </UiButton>
-      </div>
-    </div>
-    <!--  Pane 1 -->
-    <div class="pane">
-      <div class="pane__content">
-        <slot :name="`pane-${panes[1]}`" />
-      </div>
-      <div class="pane__controls">
-        <UiButton variant="secondary" block @click="up(1)"> Up </UiButton>
-        <UiButton variant="danger" block @click="down(1)"> Down </UiButton>
-      </div>
-    </div>
-    <!--  Pane 2 -->
-    <div class="pane">
-      <div class="pane__content">
-        <slot :name="`pane-${panes[2]}`" />
-      </div>
-      <div class="pane__controls">
-        <UiButton variant="secondary" block @click="up(2)"> Up </UiButton>
-        <UiButton class="pane__disabled-button" variant="danger" block @click="down(2)"> Down </UiButton>
-      </div>
-    </div>
-  </div>
-</template>
-
 <script lang="jsx">
 // Предлагается решать задачу с использованием JSX, но вы можете использовать и чистые рендер-функции
 
@@ -54,7 +17,7 @@ export default {
        * [0, 1, 2]
        * @type {number[]|null}
        */
-      panes: [0, 1, 2],
+      panes: null,
       // Сейчас здесь массив ровно из трёх элементов, но решение должно быть универсальным для любого количества узлов
     };
   },
@@ -81,6 +44,43 @@ export default {
       this.panes[i] = this.panes[i + 1];
       this.panes[i + 1] = temp;
     },
+  },
+
+  created() {
+    this.panes = this.$slots.default?.();
+  },
+
+  render() {
+    return (
+      <div class="panes">
+        {this.panes.map((vnode, index) => {
+          return (
+            <div class="pane" key={index}>
+              <div class="pane__content">{vnode}</div>
+              <div class="pane__controls">
+                <UiButton
+                  class={index === 0 && 'pane__disabled-button'}
+                  variant="secondary"
+                  block
+                  onClick={() => this.up(index)}
+                >
+                  Up
+                </UiButton>
+
+                <UiButton
+                  class={index === this.panes.length - 1 && 'pane__disabled-button'}
+                  variant="danger"
+                  block
+                  onClick={() => this.down(index)}
+                >
+                  Down
+                </UiButton>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    );
   },
 };
 </script>
