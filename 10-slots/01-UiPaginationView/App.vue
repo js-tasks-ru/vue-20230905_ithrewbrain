20d9244d6346ee6fa53b0cc35e1b9df8cf3e8f7e
Stepan Shtennikov <shtennikov.s@gmail.com>
2023-11-01 11:21:20 +0500

    Add solution to UiPaginationView problem

--- 10-slots/01-UiPaginationView/App.vue
+++ 10-slots/01-UiPaginationView/App.vue
@@ -7,7 +7,7 @@
     </div>
 
     <UiPaginationView v-slot="{ item }" :items="meetups" :page="page" :per-page="perPage">
-      <MeetupCard :meetup="item" class="list-item" />
+      <MeetupCard :meetup="item" :key="item.id"  class="list-item" />
     </UiPaginationView>
   </div>
 </template>
